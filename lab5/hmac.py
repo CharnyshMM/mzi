4ead004a5a4f45b47663f1da51f6d422d799b50b
Mikita Charnysh <mcharnysh@insoftgroup.com>
2019-12-04 10:21:47 +0300

    finish hmac function

--- lab5/hmac.py
+++ lab5/hmac.py
@@ -1,10 +1,23 @@
-import hashlib
+from hashlib import md5
 
 K = 123 # secret key
-hash # 
 
-BLOCK_SIZE = 8
+ # ipad = [ '\x36' * block_size ]
+    # // оператор "*" указывает количество повторений последовательности байт,
+    # // а block_size - размер блока хеш-функции, 
+    # opad = [ '\x5c' * block_size ]
+    
+    # ikeypad = ipad ⊕ key
+    # // оператор "⊕" выполняет побитовое исключающее ИЛИ (xor)
+    # okeypad = opad ⊕ key
+    
+    # RETURN hash( okeypad ∥ hash( ikeypad ∥ msg ) )
+    # // Оператор "∥" выполняет склейку строк
+
 def hmac(key, msg):
+    # key & message are expected to be bytearrays
+    hash = md5()
+    BLOCK_SIZE = hash.block_size
     if len(key) > BLOCK_SIZE:
         key = hash(key) 
     
@@ -17,16 +30,9 @@ def hmac(key, msg):
     ikeypad = [ipad[i] ^ key[i] for i in range(BLOCK_SIZE)]
     okeypad = [opad[i] ^ key[i] for i in range(BLOCK_SIZE)]
 
-    # ipad = [ '\x36' * block_size ]
-    # // оператор "*" указывает количество повторений последовательности байт,
-    # // а block_size - размер блока хеш-функции, 
-    # opad = [ '\x5c' * block_size ]
-    
-    # ikeypad = ipad ⊕ key
-    # // оператор "⊕" выполняет побитовое исключающее ИЛИ (xor)
-    # okeypad = opad ⊕ key
-    
-    # RETURN hash( okeypad ∥ hash( ikeypad ∥ msg ) )
-    # // Оператор "∥" выполняет склейку строк
-    return hash()
-    
+    h1.update(bytearray(ikeypad) + sb) 
+    h2 = hashlib.md5()
+    h2.update(bytearray(okeypad) + h2.digest())
+    return h2.digest()
+
+   
