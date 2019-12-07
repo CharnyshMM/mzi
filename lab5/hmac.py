6d945b9a101f8321243a914be9113f95843f218a
Mikita Charnysh <mr.nick.cher@gmail.com>
2019-12-07 21:22:45 +0300

    it works!

--- lab5/hmac.py
+++ lab5/hmac.py
@@ -1,4 +1,5 @@
 from hashlib import md5
+import random
 
 K = 123 # secret key
 
@@ -19,10 +20,10 @@ def hmac(key, msg):
     hash = md5()
     BLOCK_SIZE = hash.block_size
     if len(key) > BLOCK_SIZE:
-        key = hash(key) 
+        key = hash.update(key).digest 
     
     if len(key) < BLOCK_SIZE:
-        key = key + "0"*(BLOCK_Slen(key))
+        key = key + bytearray([0]*(BLOCK_SIZE))
 
     ipad = [0x36] * BLOCK_SIZE
     opad = [0x5c] * BLOCK_SIZE
@@ -30,9 +31,23 @@ def hmac(key, msg):
     ikeypad = [ipad[i] ^ key[i] for i in range(BLOCK_SIZE)]
     okeypad = [opad[i] ^ key[i] for i in range(BLOCK_SIZE)]
 
-    h1.update(bytearray(ikeypad) + sb) 
-    h2 = hashlib.md5()
+    h1 = md5()
+    h1.update(bytearray(ikeypad) + msg) 
+    h2 = md5()
     h2.update(bytearray(okeypad) + h2.digest())
     return h2.digest()
 
-   
+def run():
+    message = "1234567812345678901234567890"
+    message_b = bytearray(message, "utf-8")
+    # just a test of coverting to & from bytes :)
+    key = bytearray([random.randint(0, 255) for i in range(64)])
+    int_key = int.from_bytes(key, byteorder='big', signed=False)
+    key = bytearray(int.to_bytes(int_key, byteorder='big', signed=False, length=64))
+    
+    h1 = hmac(message_b, key)
+    print(len(h1))
+    print(int.from_bytes(h1, byteorder='big', signed=False))
+
+if __name__ == "__main__":
+    run()
